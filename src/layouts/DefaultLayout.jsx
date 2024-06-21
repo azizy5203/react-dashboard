02c22d3dfe2f71b8d0b45c816e00bdd5be9e7ce9
Youssef Abdulaziz <azizy5203@gmail.com>
2024-06-21 10:02:18 +0300

    style : 404 size

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -18,7 +18,7 @@ const DefaultLayout = () => {
   // Default fallback if no route matches
   return (
     <div>
-      <h1>404 - Not Found</h1>
+      <h1 className="text-4xl">404 - Not Found</h1>
     </div>
   );
 };
